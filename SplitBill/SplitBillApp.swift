f34e4827090c22feb4c3b6f151eddcf6f0474808
romanmuni8 <romanmuni8@gmail.com>
2022-10-13 15:38:36 -0400

    Initial Commit

---
+++ SplitBill/SplitBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitBillApp.swift
+//  SplitBill
+//
+//  Created by Roman on 10/13/22.
+//
+
+import SwiftUI
+
+@main
+struct SplitBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
